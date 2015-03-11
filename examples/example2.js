d31bdcfa28b97e656224e50a96cdc232b5294a94
James Westgate <jameswestgate@icloud.com>
2015-03-11 18:52:00 +0000

    Improve readability of example 2

--- examples/example2.js
+++ examples/example2.js
@@ -1,6 +1,6 @@
 (function() {
 
-	var model = {
+	var modelA = {
 		firstName: 'Robert',
 		initial: 'M.',
 		lastName: 'White',
@@ -19,31 +19,32 @@
 		tokens = writer.parse(template);
 
 	//Create a context from the first model 
-	var context = new Mustache.Context(model);
+	var context = new Mustache.Context(modelA);
 
 	//Render markup and apply to target element
 	var html = writer.renderTokens(tokens, context);
 
 	//Replace html
 	element.innerHTML = html;
-
-	//Define new pilot data
-	var modelB = {
-		firstName: 'Joseph',
-		initial: 'A',
-		lastName: 'Walker',
-		flight: 77,
-		date: '17 January 1963',
-		altitude: 51.4,
-		roundAltitude: function() {
-			return Math.round(this.altitude);
-		}
-	}
+	
 
 	//When the button is clicked, render the new model and apply the changes to the DOM
 	var button = document.getElementsByTagName('button')[0];
 
 	button.addEventListener('click', function() {
+
+		//Define new pilot data
+		var modelB = {
+			firstName: 'Joseph',
+			initial: 'A',
+			lastName: 'Walker',
+			flight: 77,
+			date: '17 January 1963',
+			altitude: 51.4,
+			roundAltitude: function() {
+				return Math.round(this.altitude);
+			}
+		}
 
 		context = new Mustache.Context(modelB);
 		html = writer.renderTokens(tokens, context);
